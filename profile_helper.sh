eb1662802ccfd01ef26c4df2298d010dc4e73cbf
Roberto Sales <robertosalesc@dcc.ufba.br>
2016-07-04 14:04:01 -0300

    update profile

--- profile_helper.sh
+++ profile_helper.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
+vim16_dir=~/.vim/bundle/base16-vim/colors
 script_dir=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
 for script in $script_dir/base16*.sh; do
   script_name=$(basename $script .sh)
-  theme=${script_name#*-}
-  variation=${theme#*.}
-  theme=${theme%.*}
+  variation=dark
+  if [[ $script_name == *"light"* ]]; then
+    variation=light
+  fi
   cat <<FUNC
-base16_${theme}_${variation}()
+$script_name()
 {
   . $script
   ln -fs $script ~/.base16_theme 
-  export BASE16_THEME=base16-${theme}
+  export BASE16_THEME=$script_name
   export BASE16_VARIATION=$variation
   echo $variation > ~/.base16_variation.rc
   if type tmux_${variation} >/dev/null; then
@@ -18,8 +20,8 @@ base16_${theme}_${variation}()
   fi
   [ -f ~/.vimrc_background ] && rm ~/.vimrc_background
   cat <<VIMRC_BACKGROUND > ~/.vimrc_background
-set background=$variation
-colorscheme base16-$theme
+"set background=$variation
+colorscheme $script_name
 VIMRC_BACKGROUND
 }
 FUNC
